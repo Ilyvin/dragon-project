ef59cd78b9b64db564ef6c765f813ec90487e3a5
Ilya Korovin <ilyvin@yandex.ru>
2020-04-15 22:25:46 +0300

    2. Расширение сцены (добавление моделей, усложнение) * оставлять следы пуль на стенах, дверях. Следы должны пропадать через 10с.

---
+++ part_01_base_settings/Assets/Scripts/BulletProjector/BulletProjectorController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletProjectorController : MonoBehaviour {
+	
+    public float lifetime = 5f;
+
+    void Start() 
+    {
+        Destroy(gameObject, lifetime);
+    }
+}
