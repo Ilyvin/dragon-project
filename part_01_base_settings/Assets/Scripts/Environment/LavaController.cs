fa3ed773021f77e0f31812437254fa234604bf5f
Ilya Korovin <ilyvin@yandex.ru>
2020-05-21 20:18:54 +0300

    * Лава

---
+++ part_01_base_settings/Assets/Scripts/Environment/LavaController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaController : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public int damage = 5;
+    public float nextTimeToDamage = 0f;
+    public float damageInterval = 0.5f;
+    private IEnumerator attackingProcessCoroutine;
+    private PlayerHealthController playerHealth;
+    
+    private void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.Play();
+        attackingProcessCoroutine = attackingProcess(damageInterval);
+    }
+    
+    /*private void Update()
+    {
+        nextTimeToDamage = Time.time + damageInterval;
+    }*/
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("AmmoItem");
+        if (other.gameObject.tag == "Player")
+        {
+
+            playerHealth = other.gameObject.GetComponent<PlayerHealthController>();
+            StartCoroutine(attackingProcessCoroutine);
+            /*while (true)
+            {
+                if (Time.time >= nextTimeToDamage)
+                {
+                    nextTimeToDamage = Time.time + damageInterval;
+                    Debug.Log("Player's ammo was decreased by lava: " + damage);
+                    other.gameObject.GetComponent<PlayerHealthController>().changeHealth(-damage);
+                }
+            }*/
+        }
+    }
+    
+    private IEnumerator attackingProcess(float waitTime)
+    {
+        while (true)
+        {
+            //if (Time.time >= nextTimeToDamage)
+            {
+                //nextTimeToDamage = Time.time + damageInterval;
+                Debug.Log("Player's ammo was decreased by lava: " + damage);
+                playerHealth.changeHealth(-damage);
+            }
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StopCoroutine(attackingProcessCoroutine);
+        }
+    }
+}
