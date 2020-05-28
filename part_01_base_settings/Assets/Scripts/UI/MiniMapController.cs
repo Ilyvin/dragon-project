309aebaaf8afc18dc51e23a9610de2fbc81b75b5
Ilya Korovin <ilyvin@yandex.ru>
2020-05-28 12:13:09 +0300

    * создать мини-карту

---
+++ part_01_base_settings/Assets/Scripts/UI/MiniMapController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapController : MonoBehaviour
+{
+    public Transform player;
+    public bool staticMap = false;
+    void LateUpdate()
+    {
+        Vector3 newPosition = new Vector3(player.position.x, transform.position.y, player.position.z);
+        transform.position = newPosition;
+
+        if (!staticMap)
+        {
+            transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+        }
+    }
+}
